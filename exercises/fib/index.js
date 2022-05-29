60899c3068915e0a0da01bad75e7fa795534e745
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-05-29 12:38:15 +0530

    add sol to fib exercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,23 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    const fibseries = [0];
+    let prevFib = 1;
+    let fib = 0;
+    let prevPrevFib = 0;
+    for (let i = 1; i <= n; i++) {
+        if(i===1) {
+            fibseries.push(1);
+        }else {
+        fib = prevFib + prevPrevFib;
+        prevPrevFib = prevFib;
+        prevFib = fib
+        fibseries.push(fib);
+        }
+    }
+    return fibseries[n];
+}
+
 
 module.exports = fib;
