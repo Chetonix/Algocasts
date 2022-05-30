56b5105cc42af627f75c87c51974ca7b1196131b
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-05-30 12:13:58 +0530

    add sol to qfroms exercise

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,44 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.myQueue1 = new Stack;
+        this.myQueue2 = new Stack;
+
+    }
+
+    add(record) {
+        this.myQueue1.push(record);
+    }
+
+    remove() {
+        while (this.myQueue1.peek()) {
+            this.myQueue2.push(this.myQueue1.pop());
+        }
+
+        const record = this.myQueue2.pop();
+
+        while(this.myQueue2.peek()) {
+            this.myQueue1.push(this.myQueue2.pop());
+        }
+        return record;
+
+    }
+
+    peek() {
+        while(this.myQueue1.peek()) {
+            this.myQueue2.push(this.myQueue1.pop());
+        }
+
+        const record = this.myQueue2.peek();
+
+        while(this.myQueue2.peek()) {
+            this.myQueue1.push(this.myQueue2.pop());
+        }
+        
+        return record;
+    }
+}
 
 module.exports = Queue;
