31c0cbe16db86960980279c68524e2409d877394
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-02 16:38:11 +0530

    add sol to levelwidth exercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -9,8 +9,44 @@
 // 1   2   3
 // |       |
 // 4       5
-// Answer: [1, 3, 2]
+// Answer: [1, 3, 2
 
-function levelWidth(root) {}
+const Node = require('./node');
+
+function levelWidth(root) {
+    let baseArray = [root];
+    let childrenArray = baseArray[0].children;
+    let counts = [1];
+
+    
+    while (childrenArray.length) {
+        
+        baseArray = [...childrenArray];
+        console.log("baseArray", baseArray);
+        childrenArray = [];
+        counts.push(baseArray.length);
+        while(baseArray.length) {
+            let removedNode = baseArray.pop();
+            console.log("removedNode:",removedNode);
+            console.log("removedNode children: ", removedNode.children);
+            
+                if (removedNode.children === undefined) {
+                    break;
+                }
+
+                if (removedNode.children.length) {
+                    childrenArray.push(...removedNode.children);
+                }
+            console.log("baseArray: ", baseArray);
+            console.log("childrenArray: ", childrenArray);
+            
+        }
+        
+        console.log("counts: ", counts);
+    }
+    return counts;
+
+}
+
 
 module.exports = levelWidth;
