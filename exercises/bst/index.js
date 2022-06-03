1bcbf0017b09c5c72496498aa9b2a86401e562ad
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-03 12:05:47 +0530

    add contains method to the bst exercise

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,68 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    insert(data) {
+        // let node = new Node(data);
+        // if (node.data > this.data) {
+            
+        //     this.right = node;
+        //     console.log("right: ", this.right);
+        // }else {
+        //     this.left = node;
+        //     console.log("left:" ,this.left);
+        // }
+
+        // const node = new Node(data);
+        if (data > this.data && this.right) {
+            this.right.insert(data);
+        } else if (data > this.data) {
+            this.right = new Node(data);
+        } else if (data < this.data && this.left) {
+            this.left.insert(data);
+        } else if (data < this.data) {
+            this.left = new Node(data);
+        }
+    }
+
+    contains (data) {
+        // if (data > this.data) {
+        //     this.right.contains(data);
+        // } else if (data < this.data) {
+        //     this.left.contains(data);
+        // }else if (data === this.data) {
+        //     return this.data;
+        // }
+
+        if (data === this.data) {
+            return this;
+        }
+
+        if (data > this.data && this.right) {
+            return this.right.contains(data);
+        } else if (data < this.data && this.left) {
+            return this.left.contains(data);
+        }
+
+
+        return null;
+
+    }
+}
+
+// const node = new Node(10);
+//   node.insert(5);
+//   node.insert(15);
+//   node.insert(17);
+
+//   console.log(node.left);
+
 
 module.exports = Node;
