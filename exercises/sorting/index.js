7d42a88880fe0fcc4a8e8a3daf7bde3cb066a402
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-04 12:38:18 +0530

    add bubble sort to the sorting exercise

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,6 +2,20 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            let swap = 0;
+            if (arr[j+1] < arr[j]) {
+                swap = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = swap;
+            }
+        }
+
+    }
+
+    return arr;
+
 
 }
 
