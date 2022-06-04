48968c685e78c5aa1e6fcaa6b9e7ac0ecb879796
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-04 13:24:00 +0530

    add selection sort to the sorting exercise

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -20,7 +20,20 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            } 
+        }
+        if (indexOfMin !== i) {
+            let swap = arr[i];
+            arr[i] = arr[indexOfMin];
+            arr[indexOfMin] = swap;
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
