77a9c2df99a78b28c6f07932d763a8eb05fcfec8
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-04 14:12:20 +0530

    add mergeSort fn to the sorting exercise

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -38,9 +38,30 @@ function selectionSort(arr) {
 
 function mergeSort(arr) {
 
+    if (arr.length === 1) {
+        return arr; 
+    }
+
+    const center = Math.floor(arr.length / 2 );
+    const left = arr.slice(0, center);
+    const right = arr.splice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
+
 }
 
 function merge(left, right) {
+    let results = [];
+
+    while(left.length && right.length) {
+        if(left[0] > right[0]) {
+            results.push(right.shift());
+        } else {
+            results.push(left.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
 
 }
 
